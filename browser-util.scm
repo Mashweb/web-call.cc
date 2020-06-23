2dd09bc65422e8bb1cc9e3faabfc2c4d81e54552
Tom Elam <tomelam@gmail.com>
2020-06-23 23:44:29 +0530

    Fixed a bug that was preventing the plus sign from showing up for copy operations. Made just the canvas and descendents draggable.

--- browser-util.scm
+++ browser-util.scm
@@ -139,9 +139,4 @@
 (define (message str)
   (console-log (format #f "Message: ~a" str))
   (js-set! (getelem1 "#message") "innerHTML"
-	   (string-append "<p style='background-color:#f0d060; color:#208020; font-size:2em;'>" str "</p>"))
-
-(define (error str)
-  (console-log (format #f "Error: ~a" str))
-  (js-set! (getelem1 "#error") "innerHTML"
-	   (string-append "<p style='color:#cc8080; font-size:2em;'>" str "</p>"))
+	   (string-append "<span class='message'>" str "</span>"))
