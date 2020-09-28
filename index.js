3c600ac15b62905cb0a5481b260001ce8a84e9f3
Tom Elam <tomelam@gmail.com>
2020-09-29 00:31:24 +0530

    Committing package-lock.json . Fixed Vaadin import statement.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { finder } from '@medv/finder'
-import { vaadin } from '@vaadin/vaadin'
+import '@vaadin/vaadin'
 
 console.log('@medv/finder and @vaadin/vaadin loaded');
 window.finder = finder;
